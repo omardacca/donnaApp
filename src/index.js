ee4f92ce8eb3d9b75746fc18344ba64669d12a28
omar <omar@webeyez.com>
2020-04-17 20:08:48 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express()
-const port = 3001
+const port = 80;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
